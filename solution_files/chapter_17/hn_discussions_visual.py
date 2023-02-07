6d553dafbee2f9dfaaa6aec0463517fc124900a5
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-02-07 15:17:27 -0800

    Finished 17-2 solution.

--- solution_files/chapter_17/hn_discussions_visual.py
+++ solution_files/chapter_17/hn_discussions_visual.py
@@ -13,7 +13,7 @@ print(f"Status code: {r.status_code}")
 submission_ids = r.json()
 
 submission_dicts = []
-for submission_id in submission_ids[:5]:
+for submission_id in submission_ids[:20]:
     # Make a new API call for each submission.
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
@@ -21,37 +21,40 @@ for submission_id in submission_ids[:5]:
     response_dict = r.json()
     
     # Build a dictionary for each article.
-    submission_dict = {
-        'title': response_dict['title'],
-        'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
-        'comments': response_dict['descendants'],
-    }
-    submission_dicts.append(submission_dict)
+    try:
+        submission_dict = {
+            'title': response_dict['title'],
+            'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
+            'comments': response_dict['descendants'],
+        }
+    except KeyError:
+        # This is a special YC post with comments disabled.
+        continue
+    else:
+        submission_dicts.append(submission_dict)
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                             reverse=True)
 
 # Process data for plotting.
-article_links, comment_counts = [], []
+article_links, comment_counts, hover_texts = [], [], []
 for submission_dict in submission_dicts:
     # Shorten long article titles.
     title = submission_dict['title'][:30]
     discussion_link = submission_dict['hn_link']
-    article_link = f"<a href={discussion_link}>{title}</a>"
+    article_link = f'<a href="{discussion_link}"">{title}</a>'
     comment_count = submission_dict['comments']
 
     article_links.append(article_link)
     comment_counts.append(comment_count)
-
-print(len(article_links), len(comment_counts))
-print(article_links)
-print(comment_counts)
+    # Show the full title on hover.
+    hover_texts.append(submission_dict['title'])
 
 # Make visualization.
 title = "Most active discussions on Hacker News"
 labels = {'x': 'Article', 'y': 'Comment count'}
-fig = px.bar(x=article_links, y=comment_counts)#, title=title,
-        # labels=labels) #, hover_name=hover_texts)
+fig = px.bar(x=article_links, y=comment_counts, title=title, labels=labels,
+        hover_name=hover_texts)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
         yaxis_title_font_size=20)
