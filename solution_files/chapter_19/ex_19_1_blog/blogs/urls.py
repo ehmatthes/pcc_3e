47f7d60dae78e243abc44e3c7eb75a7c8b0230c8
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-02-09 16:14:41 -0900

    Blog project has page to view all blogs, and a blog's posts.

--- solution_files/chapter_19/ex_19_1_blog/blogs/urls.py
+++ solution_files/chapter_19/ex_19_1_blog/blogs/urls.py
@@ -8,4 +8,8 @@ app_name = 'blogs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page to view all blogs that have been created.
+    path('blogs/', views.blogs, name='blogs'),
+    # Page to view an individual blog, and all its posts.
+    path('blog/<int:blog_id>/', views.blog, name='blog'),
 ]
--- solution_files/chapter_19/ex_19_1_blog/blogs/views.py
+++ solution_files/chapter_19/ex_19_1_blog/blogs/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
+from .models import Blog, BlogPost
+
 
 def index(request):
     """The home page for Blog."""
     return render(request, 'blogs/index.html')
+
+def blogs(request):
+    """Page to view all blogs that have been created."""
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blogs/blogs.html', context)
+
+def blog(request, blog_id):
+    """Page to view an individual blog, and all its posts."""
+    blog = Blog.objects.get(id=blog_id)
+    posts = blog.blogpost_set.all()
+
+    context = {'blog': blog, 'posts': posts}
+    return render(request, 'blogs/blog.html', context)
