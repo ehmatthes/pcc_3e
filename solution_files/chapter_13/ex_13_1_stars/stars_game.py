5ec61e7800d3bf748cd6a23d1ff5962372ffae5b
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-02-11 15:33:44 -0900

    Finished 13-1.

--- solution_files/chapter_13/ex_13_1_stars/stars_game.py
+++ solution_files/chapter_13/ex_13_1_stars/stars_game.py
@@ -12,6 +12,7 @@ class StarsGame:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
@@ -26,6 +27,7 @@ class StarsGame:
         while True:
             self._check_events()
             self._update_screen()
+            self.clock.tick()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -41,6 +43,32 @@ class StarsGame:
             sys.exit()
 
     def _create_stars(self):
+        """Create a sky full of stars."""
+        # Create a star and keep adding stars until there's no room left.
+        #   Spacing between stars is two star widths.
+        star = Star(self)
+        star_width, star_height = star.rect.size
+
+        current_x, current_y = 2*star_width, 2*star_height
+        while current_y < (self.settings.screen_height - 3 * star_height):
+            while current_x < (self.settings.screen_width - 2 * star_width):
+                self._create_star(current_x, current_y)
+                current_x += 2 * star_width
+
+            # Finished a row; reset x value, and increment y value.
+            current_x = 2 * star_width
+            current_y += 2 * star_height
+
+
+    def _create_star(self, x_position, y_position):
+        """Create a star and place it in the grid."""
+        new_star = Star(self)
+        new_star.x = x_position
+        new_star.rect.x = x_position
+        new_star.rect.y = y_position
+        self.stars.add(new_star)
+
+    def _create_stars_old(self):
         """Create a sky full of stars."""
         # Create an star and find the number of stars in a row.
         # Spacing between each star is equal to two star widths.
@@ -60,7 +88,7 @@ class StarsGame:
             for star_number in range(number_stars_x):
                 self._create_star(star_number, row_number)
 
-    def _create_star(self, star_number, row_number):
+    def _create_star_old(self, star_number, row_number):
         """Create an star and place it in the row."""
         star = Star(self)
         star_width, star_height = star.rect.size
