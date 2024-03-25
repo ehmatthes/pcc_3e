fcd34a6c62e9c66d628872c5e7c8488923e0fc55
Eduardo Cabrera <eduardo.cabrera@rimac.com.pe>
2024-03-25 17:01:52 -0500

    iniciando el capitulo 4

---
+++ chapter_04/PracticaPersonal/4-1 Pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ['pepperoni', 'vegetales', 'queso']
+for pizza in pizzas:
+    print(pizza)
+    print(f'me gusta la pizza de {pizza}')
+
---
+++ chapter_04/PracticaPersonal/4-2 Animals.py
@@ -0,0 +1,5 @@
+animales = ['vaca', 'gato', 'mono', 'jiraja']
+for animal in animales:
+    print(f'{animal} es un animal con cola')
+
+print('Estos animales tienen cola')
