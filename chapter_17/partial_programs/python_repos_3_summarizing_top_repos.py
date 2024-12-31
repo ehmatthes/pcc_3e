b517b4473ef2b29ae7b1cfa0e81edb85f5a2c37f
rmdavis3 <94387716+rmdavis3@users.noreply.github.com>
2024-12-31 08:13:33 -0500

    Update python_repos_3_summarizing_top_repos.py
    Incorrect print statement. This line is not in the 3rd edition book.

--- chapter_17/partial_programs/python_repos_3_summarizing_top_repos.py
+++ chapter_17/partial_programs/python_repos_3_summarizing_top_repos.py
@@ -21,7 +21,6 @@ print(f"Repositories returned: {len(repo_dicts)}")
 
 print("\nSelected information about each repository:")
 for repo_dict in repo_dicts:
-    print("\nSelected information about first repository:")
     print(f"Name: {repo_dict['name']}")
     print(f"Owner: {repo_dict['owner']['login']}")
     print(f"Stars: {repo_dict['stargazers_count']}")
