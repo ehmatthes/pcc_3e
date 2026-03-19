3834f99d428ad9a0fd6db81c4d3cd49ff1493914
Adi22 <adi@superdev.com>
2026-03-19 15:36:03 -0700

    DELETE 2 OTHER NAME FILES ALSO EXPLAINED (lower(), upper())

--- chapter_02/name.py
+++ chapter_02/name.py
@@ -1,2 +1,5 @@
-name = "ada lovelace"
-print(name.title())
+NAME = "ada lovelace"
+# THIS MAKES THE FILRST LETTER A UPPERCASE
+print(NAME.title())
+print(NAME.upper()) # MAKES THE LETTERS ALL UPPERCASE
+print(NAME.lower()) # MAKES THE LETTERS ALL lowercase
