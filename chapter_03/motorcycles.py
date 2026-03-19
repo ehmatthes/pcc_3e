77a2fbf7ad9df67ddb2c0badc982e62e5ea4b8e9
Adi <146877894+aroskey@users.noreply.github.com>
2026-03-19 14:42:02 -0700

    Refactor too_expensive assignment and remove from list
    Updated the too_expensive variable to reference the motorcycle from the list and removed it from the motorcycles list.

--- chapter_03/motorcycles.py
+++ chapter_03/motorcycles.py
@@ -1,7 +1,9 @@
 motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
 print(motorcycles)
 
-too_expensive = 'ducati'
+
+# NEED TO MAKE THE VALUE LIST 
+too_expensive = motorcycles[3]
 motorcycles.remove(too_expensive)
 print(motorcycles)
 print(f"\nA {too_expensive.title()} is too expensive for me.")
