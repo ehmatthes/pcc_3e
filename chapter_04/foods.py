9cad7d0237e11c1c4edc34186627db4d29e48578
Adi22 <adi@superdev.com>
2026-03-21 03:13:07 -0700

    ADDED ANOTHER EXMPLE

--- chapter_04/foods.py
+++ chapter_04/foods.py
@@ -1,11 +1,22 @@
-my_foods = ['pizza', 'falafel', 'carrot cake']
-friend_foods = my_foods[:]
+# FOODS SECTION
+MY_FOODS = ['pizza', 'falafel', 'carrot cake'.upper()]
+FRIEND_FOOD = MY_FOODS[:] # THIS WILL CREATE TRUE INDEPENDENT LIST WITH SAME VALUE OF THE FIRST LIST (MY_FOODS)
 
-my_foods.append('cannoli')
-friend_foods.append('ice cream')
+MY_FOODS.append('bariani'.upper()) # HERE IT ADD THIS VALUE IN THE END OF THE LIST
+FRIEND_FOOD.append('katsu kari'.upper()) 
 
 print("My favorite foods are:")
-print(my_foods)
+print(MY_FOODS)
+
+
 
 print("\nMy friend's favorite foods are:")
-print(friend_foods)
+print(FRIEND_FOOD, "\n")
+
+print(2 * "=============" * 2)
+
+my_fruits = ["apple", "orange", "banana"]
+friend_fruits = my_fruits.copy() # ALSO THIS WAY OF COPYING A LIST WORKS LIKE SAME FOODS LINE 2 => FRIEND_FOOD = MY_FOODS[:]
+friend_fruits.insert(0, "cherry")
+
+print(friend_fruits)
