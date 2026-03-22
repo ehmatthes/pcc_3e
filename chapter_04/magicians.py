c9a3b35d94c7aca760f083268066e8effb79178e
Adi22 <adi@superdev.com>
2026-03-21 22:02:11 -0700

    CHANGE THE SLICE INDEX

--- chapter_04/magicians.py
+++ chapter_04/magicians.py
@@ -1,5 +1,5 @@
 magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
+for magician in magicians[1:3]: # IT WILL JUST PRINTS FOR DAVID AND CAROLINA 
     print(f"{magician.title()}, that was a great trick!")
     print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
